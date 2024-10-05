57fe43a33b95e299cfda598f9237f3f7d3936fb7
kimiyash <kimiyash@gmail.com>
2024-10-05 13:05:44 +0900

    ch02

--- ch02/examples/fraction.rs
+++ ch02/examples/fraction.rs
@@ -26,6 +26,13 @@ impl Fraction {
 impl Add for Fraction {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
+        // // お互いの分母を書けて最大公約数で割ることで分母の最小公倍数を求める
+        // let dlcm = self.1 * rhs.1 / Self::gcd(self.1, rhs.1);
+        // // 最小公倍数を分母で割った数で分子に掛ける
+        // let numerator = self.0 * (dlcm / self.1) + rhs.0 * (dlcm / rhs.1);
+        // Fraction::new(numerator, dlcm)
+
+        // どうせ約分するんだしこっちでよくない？
         let denominator = self.1 * rhs.1;
         let numerator = self.0 * rhs.1 + rhs.0 * self.1;
         Fraction::new(numerator, denominator)
@@ -42,14 +49,15 @@ fn main() {
     assert_eq!(Fraction::new(10, 30), Fraction::new(1, 3));
     assert_eq!(Fraction::new(30, 10), Fraction::new(3, 1));
     println!("{}", Fraction::new(56, 104));
+    assert_eq!(Fraction::new(56, 104), Fraction::new(7, 13));
+    println!("{}", Fraction::new(10, 30) + Fraction::new(30, 10));
     assert_eq!(
         Fraction::new(10, 30) + Fraction::new(30, 10),
         Fraction::new(100, 30)
     );
-    println!("{}", Fraction::new(10, 30) + Fraction::new(30, 10));
+    println!("{}", Fraction::new(2, 3) + Fraction::new(4, 5));
     assert_eq!(
         Fraction::new(2, 3) + Fraction::new(4, 5),
         Fraction::new(22, 15)
     );
-    println!("{}", Fraction::new(2, 3) + Fraction::new(4, 5));
 }
